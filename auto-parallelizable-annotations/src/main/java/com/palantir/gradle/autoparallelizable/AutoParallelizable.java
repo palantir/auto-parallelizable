54878e7e81efdb8d032dae54bb0ec21e18669158
Callum Rogers <callumr@palantir.com>
2022-12-16 17:21:36 +0000

    source retention

--- auto-parallelizable-annotations/src/main/java/com/palantir/gradle/autoparallelizable/AutoParallelizable.java
+++ auto-parallelizable-annotations/src/main/java/com/palantir/gradle/autoparallelizable/AutoParallelizable.java
@@ -16,4 +16,8 @@
 
 package com.palantir.gradle.autoparallelizable;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
 public @interface AutoParallelizable {}
